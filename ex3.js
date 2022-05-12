7a7e5a8b651680cb9e84d2b8a29daa46bbafbe17
Carolina Lisa <carolina.lisa.ferrer@gmail.com>
2022-05-12 18:42:54 +0200

    first commit

---
+++ ex3.js
@@ -0,0 +1,28 @@
+/**
+ * Jugando con string
+ */
+
+let nombre = "Mad";
+let apellido = "Max";
+
+let presentacion = "Me llamo" + nombre + apellido + "?";
+
+/**
+ * Pregunta 1: ¿Qué valor tengo que asignar a 'presenetacion' para que me muestre pantalla:
+ *   * "Me llamo Mad Max?"
+ *
+ * 
+ * 
+ */
+
+console.log("Pregunta 1:", presentacion);
+
+/**
+ * Pregunta 2: Cambia el valor de las variables adecuadamente para que, en vez de "Me llamo Mad Max", escribe "Me llamo {tu nombre} {tu apellido}"
+ */
+
+nombre= "Carolina";
+apellido="Lisa";
+presentacion=  "Me llamo" + " " + nombre + " " + apellido;
+
+console.log("Pregunta 2:", presentacion);
---
+++ ex4.js
@@ -0,0 +1,31 @@
+/**
+ * ¡LA ADIVINADORA!
+ * 
+ * Crea 4 variables para almacenar los siguientes valores:
+ * 
+ * 1. Número de gatos
+ * 2. Tu próxima pareja
+ * 3. Un país donde te gustaría vivir
+ * 4. Un trabajo de ensueño
+ * 
+ * A cada variable, asígnale un valor adecuado (piensa si tiene que ser un número o una cadena de texto)
+ * 
+ * Finalmente, por pantalla, la Adivinadora debe pronosticar tu futuro deseado de esta manera
+ * 
+ * "Tú estarás en {pais} trabajando de {trabajo} y tu pareja será {pareja} y tendréis {numeroGatos} gatos" 
+ * 
+ */
+
+let numeroGatos = 10;
+let tuProximaPareja = "Un hombre ideal";
+let paisDondeTeGustariaVivir = "Tailandia";
+let unTrabajoDeEnsueño = "El trabajo ideal";
+
+let futuro = "tendréis " + numeroGatos + " gatos";
+let futuraPareja = "tendré" + " " + tuProximaPareja;
+let futuropaisDondeTeGustariaVivir = "viviré" + " " + "en" + " " + paisDondeTeGustariaVivir;
+let futurounTrabajodeEnsueño = "trabajaré" + " " + "en" + " " + unTrabajoDeEnsueño;
+console.log(futuro);
+console.log(futuraPareja);
+console.log(futuropaisDondeTeGustariaVivir);
+console.log(futurounTrabajodeEnsueño);
